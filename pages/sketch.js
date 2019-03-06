f896cc9823b0ee653c8f0c8062906c48368796f4
Flowmaster <ellyssin.gimhae@gmail.com>
2019-03-05 21:39:28 -0700

    Correct p5 wrapper import.

--- pages/sketch.js
+++ pages/sketch.js
@@ -1,25 +1,32 @@
 import Layout from '../components/layout'
 import Asteroids from '../sketches/asteroids/sketch'
-import P5Wrapper from '../components/p5Wrapper'
+import P5Wrapper from '../components/P5Wrapper'
 import checkLoggedIn from '../lib/checkLoggedIn'
+import redirect from '../lib/redirect'
 
 export default class Sketch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-			sketch: Asteroids
+		  sketch: Asteroids
 		};
   }
 
   static async getInitialProps (context) {
     const { loggedInUser } = await checkLoggedIn(context.apolloClient)
+    if (!loggedInUser.getUser) {
+      // Not signed in.
+      // Throw them back to the main page
+      redirect(context, '/')
+    }
+
     return { user: loggedInUser.getUser }
   }
 
   render() {
     return (
       <Layout {...this.props}>
-        <P5Wrapper sketch={this.state.sketch} />
+        <P5Wrapper sketch={this.state.sketch} /> 
       </Layout>
     );
   }
