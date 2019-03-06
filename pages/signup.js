fe7217f9462d0ff0f0b4fe980bb75c0518b439b5
Flowmaster <ellyssin.gimhae@gmail.com>
2019-03-05 20:49:11 -0700

    Redirect to index from signup and forgot page when a user is logged in.

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,21 @@
 import { Row, Col } from 'antd';
 import Layout from '../components/layout'
 import Signup from '../components/forms/signup'
+import checkLoggedIn from '../lib/checkLoggedIn'
+import redirect from '../lib/redirect'
 
 export default class SignUp extends React.Component {
+  static async getInitialProps (context) {
+    const { loggedInUser } = await checkLoggedIn(context.apolloClient)
+    if (loggedInUser.getUser) {
+      // Already signed in? No need to continue.
+      // Throw them back to the main page
+      redirect(context, '/')
+    }
+
+    return {}
+  }
+
   render = () => {
     return (
       <Layout {...this.props}>
