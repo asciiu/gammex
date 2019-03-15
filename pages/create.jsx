5d070f04793de8cd7ee83dcc3553ab4ab40c93ba
Flowmaster <ellyssin.gimhae@gmail.com>
2019-03-14 18:53:09 -0600

    Added spikes and btc.

--- pages/create.jsx
+++ pages/create.jsx
@@ -20,8 +20,10 @@ export default class CreateJesuis extends React.Component {
   }
 
   btc;
+  bubble;
   canvas;
   clouds = [];
+  spikes = [];
   squeakID = "squeak";
   bubbleID = "bubble";
   popID = "pop";
@@ -33,28 +35,51 @@ export default class CreateJesuis extends React.Component {
 
   handleComplete = () => {
     //this.btc = new createjs.Bitmap("/static/clouds/btc.png");
-    this.btc = new createjs.Bitmap("/static/clouds/bubble.png");
-    this.btc.scaleX = 0.1;
-    this.btc.scaleY = 0.1;
-    const cloud1 = new createjs.Bitmap("/static/clouds/cloud1.png");
-    const cloud2 = new createjs.Bitmap("/static/clouds/cloud1.png");
-    const cloud3 = new createjs.Bitmap("/static/clouds/cloud2.png");
-    const cloud4 = new createjs.Bitmap("/static/clouds/cloud3.png");
-    const cloud5 = new createjs.Bitmap("/static/clouds/cloud1.png");
-    const cloud6 = new createjs.Bitmap("/static/clouds/cloud4.png");
-    const cloud7 = new createjs.Bitmap("/static/clouds/cloud1.png");
-    this.clouds = [cloud1, cloud2, cloud3, cloud4, cloud5, cloud6, cloud7]
+    for (let i = 1; i < 10; i++) {
+      const spike = new createjs.Bitmap("/static/clouds/spike.png");
+      spike.scaleX = 0.1;
+      spike.scaleY = 0.1;
+      spike.x = -this.width;
+      spike.name = "spike";
+      this.stage.addChild(spike);
+      this.spikes.push(spike);
+    }
+
+    for (let i = 1; i < 3; i++) {
+      const btc = new createjs.Bitmap("/static/clouds/btc.png");
+      btc.scaleX = 0.7;
+      btc.scaleY = 0.7;
+      btc.x = -this.width;
+      btc.name = "btc";
+      this.stage.addChild(btc);
+    }
+
+    this.bubble = new createjs.Bitmap("/static/clouds/bubble.png");
+    this.bubble.name = "bubble"
+    this.bubble.scaleX = 0.1;
+    this.bubble.scaleY = 0.1;
+    this.bubble.x = 300;
+    this.bubble.y = 300;
+    this.stage.addChild(this.bubble);
+
+    for (let i = 1; i < 3; i++) {
+      const cloud1 = new createjs.Bitmap("/static/clouds/cloud1.png");
+      this.clouds.push(cloud1)
+      const cloud2 = new createjs.Bitmap("/static/clouds/cloud2.png");
+      this.clouds.push(cloud2)
+      const cloud3 = new createjs.Bitmap("/static/clouds/cloud3.png");
+      this.clouds.push(cloud3)
+      const cloud4 = new createjs.Bitmap("/static/clouds/cloud4.png");
+      this.clouds.push(cloud4)
+    }
 
     for (const cloud of this.clouds) {
+      cloud.name = "cloud";
       // init off screen
       cloud.x = -this.width;
       //cloud.y = Math.floor(Math.random() * this.height) + 1;
       this.stage.addChild(cloud);
     }
-
-    this.btc.x = 300;
-    this.btc.y = 300;
-    this.stage.addChild(this.btc);
 
     createjs.Sound.registerSound("static/clouds/bubble.mp3", this.bubbleID);
     createjs.Sound.registerSound("static/clouds/pop.mp3", this.popID);
@@ -89,11 +114,20 @@ export default class CreateJesuis extends React.Component {
   
   handleKey = (event) => {
     if (event.keyCode == 32) {
+
+      if (this.bubble.y < this.height && this.isDead) {
+        this.isDead = false;
+      }
+
       createjs.Sound.play(this.bubbleID);
 
-      createjs.Tween.get(this.btc)
-        .to({y: this.btc.y-100}, 300, createjs.Ease.getPowOut(2))
-        .to({y: this.btc.y+1000}, 700, createjs.Ease.getPowIn(2));
+      //const y = this.bubble.y;
+      //let x = 20;
+      //if (x < 20) x = 20;
+
+      createjs.Tween.get(this.bubble)
+        .to({x: this.bubble.x + 10, y: this.bubble.y-100}, 300, createjs.Ease.getPowOut(2))
+        .to({y: this.bubble.y+1000}, 700, createjs.Ease.getPowIn(2));
     }
     event.preventDefault();
   }
@@ -110,28 +144,53 @@ export default class CreateJesuis extends React.Component {
     //}
     this.stage.update();
     
-    for (const cloud of this.clouds) {
-      if (cloud.getBounds() && cloud.x <= -cloud.getBounds().width) {
-        cloud.x = this.width;
-        cloud.y = Math.floor(Math.random() * this.height) + 1;  
-
-        const time = Math.floor(Math.random() * 15000) + 7000;
-        createjs.Tween.get(cloud)
-          .to({x: -cloud.getBounds().width}, time);
-      }
+    const bubble = this.stage.getChildByName("bubble");
+    if (bubble && bubble.y >= this.height && !this.isDead) {
+      this.handleDeath();
     }
 
-    if (this.btc) {
-      if (this.btc.y >= this.height && !this.isDead) {
-        this.handleDeath();
-      }
+    for (const child of this.stage.children) {
+      switch (child.name) {
+        case "cloud":
+          let cloud = child;
+          if (cloud.getBounds() && cloud.x <= -cloud.getBounds().width) {
+            cloud.x = Math.floor(Math.random() * this.width) + this.width; 
+            cloud.y = Math.floor(Math.random() * this.height) + 1;  
 
-      if (this.btc.y < this.height && this.isDead) {
-        this.isDead = false;
+            const time = Math.floor(Math.random() * 15000) + 7000;
+            createjs.Tween.get(cloud)
+              .to({x: -cloud.getBounds().width}, time);
+          }
+          break;
+        case "btc":
+          let btc = child;
+          if (btc.getBounds() && btc.x <= -btc.getBounds().width) {
+            btc.x = Math.floor(Math.random() * this.width * 3) + this.width;
+            btc.y = Math.floor(Math.random() * this.height) + 1;  
+
+            const time = Math.floor(Math.random() * 15000) + 7000;
+            createjs.Tween.get(btc)
+              .to({x: -btc.getBounds().width}, time);
+          }
+        case "spike":
+          let spike = child;
+          if (spike.getBounds() && spike.x <= -spike.getBounds().width) {
+            spike.x = Math.floor(Math.random() * this.width * 2) + this.width;  
+            spike.y = Math.floor(Math.random() * this.height);  
+  
+            const time = Math.floor(Math.random() * 15000) + 7000;
+            createjs.Tween.get(spike)
+              .to({x: -spike.getBounds().width}, time);
+          } else if (!this.isDead) {
+            //const intersection = ndgmr.checkRectCollision(bubble,spike);
+            //if (intersection) {
+            //  this.handleDeath();
+            //}
+          }
+        default:
       }
     }
   }
-
 
   render() {
     //const props: LayoutProps = {
